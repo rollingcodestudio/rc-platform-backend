3779bb2bc3ba4fcef70c385f78e74f1731269db1
Federico Correa <you@example.com>
2020-09-16 01:39:50 -0300

    Se agregan headers necesarios

--- src/app.js
+++ src/app.js
@@ -7,6 +7,13 @@ import indexRouter from './routes/index';
 import authControllersRouter from './routes/authentication/authControllers';
 
 const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
